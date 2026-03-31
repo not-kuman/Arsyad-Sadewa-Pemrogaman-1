e17be1499713dde814637f83aec1ece66a9e2a57
not-kuman <arsyadsadewa3@gmail.com>
2026-03-31 15:07:22 +0700

    Update Projek p2 Latihan1.java

--- Projek p2/Source Package/L1/Latihan1.java
+++ Projek p2/Source Package/L1/Latihan1.java
@@ -1,39 +1,15 @@
 import java.util.Scanner;
-
 public class Latihan1 {
 
-    public static void main(String[] args){
-        Scanner inputScanner = new Scanner(System.in);
-        System.out.println(" Selamat Datang!!");
-        System.out.print(" Masukkan Nama Lengkap: ");
-        String NamaLengkap = inputScanner.nextLine();
-        int Umur = 0;
-        while (true){
-            System.out.print(" Masukkan Umur: ");
-            if (inputScanner.hasNextInt()){
-                Umur = inputScanner.nextInt();
-                inputScanner.nextLine();
-                break;
-            } else {
-                System.out.println(" Mohon masukkan angka!! ");
-                inputScanner.nextLine();
-            }
-        }
-        System.out.print("Masukkan Nama Universitas: ");
-        String Universitas = inputScanner.nextLine();
+    public static void main(String args[]){
+        Scanner input = new Scanner(System.in);
+            String nama; int usia;
+            System.out.print(" Input Nama: ");
+            nama = input.nextLine();
+            System.out.print("Input Umur");
+            usia = input.nextInt();
 
-        System.out.print(" Masukkan Progam Studi: ");
-        String ProgamStudi = inputScanner.nextLine();
-
-        System.out.println("\n========================================");
-        System.out.println("      Detail Informasi     ");
-        System.out.println("========================================");
-        System.out.printf(" => Nama: %s\n ", NamaLengkap);
-        System.out.printf(" => Umur: %d\n ", Umur);
-        System.out.printf(" => Universitas: %s\n", Universitas);
-        System.out.printf(" => Progam Study: %s\n", ProgamStudi);
-        System.out.println("========================================");
-        inputScanner.close();
-
+            System.out.print("Nama : " + nama);
+            System.out.print("Umur : "+ usia);
     }
 }
