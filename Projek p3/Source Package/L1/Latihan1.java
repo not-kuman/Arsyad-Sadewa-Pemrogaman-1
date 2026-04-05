d57c278a616a20582e78deb0e26f8478f05f2add
not-kuman <arsyadsadewa3@gmail.com>
2026-04-05 15:18:38 +0700

    Add Projek p3

---
+++ Projek p3/Source Package/L1/Latihan1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Latihan1 {
+
+    public static void main(String args[]){
+        Scanner input = new Scanner(System.in);
+            String nama; int usia;
+            System.out.print(" Input Nama: ");
+            nama = input.nextLine();
+            System.out.print("Input Umur");
+            usia = input.nextInt();
+
+            int TahunLahir = 2026 - usia;
+
+            System.out.print("Nama : " + nama);
+            System.out.print("Umur : "+ usia);
+            System.out.print("Tahun Lahir : " + TahunLahir);
+    }
+}
