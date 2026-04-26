1d46a2ea0018793c3477553eec98ff20edf4b1d3
not-kuman <arsyadsadewa3@gmail.com>
2026-04-26 13:36:10 +0700

    move files

--- Projek p8/Source Package/L1/Latihan1.java
+++ Projek p8/Source Package/L1/Latihan1.java
@@ -1,3 +1,7 @@
 public class Latihan1 {
-    
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Angka ke-" + i);
+        }
+    }
 }
